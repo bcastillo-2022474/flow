231a26b409adfe9f2724f02f78de0ccba3d8efe1
Restyled.io <commits@restyled.io>
2023-07-20 18:08:53 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                regular: [
-                    "Inter UI",
-                    "SF Pro Display",
-                    "-apple-system",
-                    "BlinkMacSystemFont",
-                    "Segoe UI",
-                    "Roboto",
-                    "Oxygen",
-                    "Ubuntu",
-                    "Cantarell",
-                    "Open Sans",
-                    "Helvetica Neue",
-                    "sans-serif",
-                ],
-            },
-        },
+  content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}" ],
+  theme: {
+    extend: {
+      fontFamily: {
+        regular:
+            [
+              "Inter UI",
+              "SF Pro Display",
+              "-apple-system",
+              "BlinkMacSystemFont",
+              "Segoe UI",
+              "Roboto",
+              "Oxygen",
+              "Ubuntu",
+              "Cantarell",
+              "Open Sans",
+              "Helvetica Neue",
+              "sans-serif",
+            ],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
