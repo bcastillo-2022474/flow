210daad919baba28c9b9deb4d95dd97c7736aff9
Restyled.io <commits@restyled.io>
2023-07-20 18:09:02 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}" ],
-  theme: {
-    extend: {
-      fontFamily: {
-        regular:
-            [
-              "Inter UI",
-              "SF Pro Display",
-              "-apple-system",
-              "BlinkMacSystemFont",
-              "Segoe UI",
-              "Roboto",
-              "Oxygen",
-              "Ubuntu",
-              "Cantarell",
-              "Open Sans",
-              "Helvetica Neue",
-              "sans-serif",
-            ],
-      },
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                regular: [
+                    "Inter UI",
+                    "SF Pro Display",
+                    "-apple-system",
+                    "BlinkMacSystemFont",
+                    "Segoe UI",
+                    "Roboto",
+                    "Oxygen",
+                    "Ubuntu",
+                    "Cantarell",
+                    "Open Sans",
+                    "Helvetica Neue",
+                    "sans-serif",
+                ],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
